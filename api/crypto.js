fbab7ad29c96f9a203250a278802fbc4d5209e06
acquireness-cmd <acquireness@gmail.com>
2026-03-01 21:47:18 +0530

    Create crypto.js

---
+++ api/crypto.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/global"
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+}
